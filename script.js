70613c15f144e7b07f5519da71c4985274a1c043
saifkhan71990 <blackud777@yahoo.in>
2025-08-06 23:27:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+/*box Content*/
+
+function onclickfun1()
+{
+    document.getElementById('box1').style.border="red"
+    document.getElementById('box1').style.background="red"
+         
+}
+
+
+function onclickfun2()
+{    
+    document.getElementById('box2').style.border="blue"
+    document.getElementById('box2').style.background="blue"
+       
+}
+
+function onclickfun3()
+{    
+    document.getElementById('box3').style.border="green"
+    document.getElementById('box3').style.background="green"
+        
+}
+
+
+function onclickfun4()
+{    
+    document.getElementById('box4').style.border="yellow"
+    document.getElementById('box4').style.background="yellow"
+       
+}
+
+
+
+const myBox1=document.getElementById('box1');
+const myBox2=document.getElementById('box2');
+const myBox3=document.getElementById('box3');
+const myBox4=document.getElementById('box4');
+
+myBox1.addEventListener('click',onclickfun1);
+myBox2.addEventListener('click',onclickfun2);
+myBox3.addEventListener('click',onclickfun3);
+myBox4.addEventListener('click',onclickfun4);
+
+
+
+
+/*form Content*/
+
+
+function onbtnclick()
+{
+    txt=document.getElementById('user').value;
+    document.getElementById("herdertext").innerText ="Hello"+" "+txt;
+    
+}
+
+
+
+function cleartext()
+{
+    document.getElementById('user').value="";
+   
+}
+
+
+const myButton=document.getElementById('mybtn');
+myButton.addEventListener('click',onbtnclick);
+myButton.addEventListener('click',cleartext);
+
